c108fc94d20f66d6ae2f0b7189d459a09a6346ad
Brandon Weaver <a7xncob@gmail.com>
2019-11-07 21:06:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,7 +51,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |holiday, supplies|
-    puts holiday
+    puts "#{holiday.to_s}"
   end
 end
 
