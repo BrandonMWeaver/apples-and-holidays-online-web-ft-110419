f813391c6c74433f252467a43c19e67768d6aac7
Brandon Weaver <a7xncob@gmail.com>
2019-11-07 21:02:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,7 +22,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  holiday_hash[:winter].each do |holiday, supplies|
+  holiday_hash[:winter].each do |holiday|
     holiday << supply
   end
   return holiday_hash
