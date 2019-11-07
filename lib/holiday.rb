a7226ec3c477679a7abf5a498d696562d106dc2b
Brandon Weaver <a7xncob@gmail.com>
2019-11-07 21:27:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,10 +61,10 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_array = []
   holiday_hash.each do |season, holidays|
-    holidays.each do |supplies|
+    holidays.each do |holiday, supplies|
       binding.pry
       if supplies.include?("BBQ")
-        holiday_array << holidays
+        holiday_array << holiday
       end
     end
   end
