896c65825c6ad10ae2b163f56ccb34a83199be92
Brandon Weaver <a7xncob@gmail.com>
2019-11-07 21:20:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
       holiday_array.each do |word|
         word.capitalize!
       end
-      holiday_string = holiday_array.join
+      holiday_string = holiday_array.join(' ')
       puts "  #{holiday_string}: #{supplies.join(", ")}"
     end
   end
