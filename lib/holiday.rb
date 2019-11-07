e053dd7571e61b933cf22f2cf42fb087fcfa9cb8
Brandon Weaver <a7xncob@gmail.com>
2019-11-07 20:56:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,6 +28,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash[:winter].each do |holiday|
     holiday << supply
   end
+  return holiday_hash
 end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
